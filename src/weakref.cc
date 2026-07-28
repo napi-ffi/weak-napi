53ebb7df423ebe9e48b411960e24b8b4297c8e04
Gilson Urbano <guf@hotmail.com.br>
2026-07-28 15:38:15 +0000

    Fix WeakTag type confusion via unchecked ObjectInfo::Unwrap

--- src/weakref.cc
+++ src/weakref.cc
@@ -1,9 +1,20 @@
 #include <napi.h>
 #include <setimmediate.h>
 
+#if NAPI_VERSION < 8
+#error "weak-napi requires NAPI_VERSION >= 8 (napi_type_tag_object support), \
+which is used to safely validate ObjectInfo instances passed to WeakTag"
+#endif
+
 using namespace Napi;
 
 namespace {
+
+// Identifies JS objects that were actually constructed as ObjectInfo, so
+// WeakTag can refuse to Unwrap() objects of any other type (see GH #1).
+constexpr napi_type_tag kObjectInfoTypeTag = {
+  0x8f6f6a6f5a3b4c1aULL, 0x9d2e7c9b1f4a6e05ULL
+};
 
 class ObjectInfo : public ObjectWrap<ObjectInfo> {
  public:
@@ -12,6 +23,7 @@ class ObjectInfo : public ObjectWrap<ObjectInfo> {
       throw Error::New(Env(), "target should be object");
     if (!args[1].IsFunction())
       throw Error::New(Env(), "callback should be function");
+    Value().TypeTag(&kObjectInfoTypeTag);
     Ref();
     target_.Reset(args[0].As<Object>(), 0);
     callback_.Reset(args[1].As<Function>(), 1);
@@ -42,8 +54,11 @@ class ObjectInfo : public ObjectWrap<ObjectInfo> {
 class WeakTag : public ObjectWrap<WeakTag> {
  public:
   WeakTag(const CallbackInfo& args) : ObjectWrap(args) {
-    if (args[0].IsObject())
-      info_ = ObjectInfo::Unwrap(args[0].As<Object>());
+    if (args[0].IsObject()) {
+      Object obj = args[0].As<Object>();
+      if (obj.CheckTypeTag(&kObjectInfoTypeTag))
+        info_ = ObjectInfo::Unwrap(obj);
+    }
     if (info_ == nullptr)
       throw Error::New(Env(), "First argument needs to be ObjectInfo");
   }
